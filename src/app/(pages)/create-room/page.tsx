115cd72adf3e7917f15600dd435374a26b3648cc
Mohamed <74735976+amadich@users.noreply.github.com>
2026-06-11 06:00:37 +0100

    Colly Initial Version v:0.0.5.7.2.1.0

--- src/app/(pages)/create-room/page.tsx
+++ src/app/(pages)/create-room/page.tsx
@@ -196,14 +196,14 @@ export default function CreateRoomPage() {
 
         {/* Anime Character - Locked tightly to the bottom layout */}
         <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-[90%] h-[85%] z-10 select-none pointer-events-none">
-          <Image
+          {/* <Image
             src="/assets/images/anime_character.webp"
             alt="Anime Character"
             fill
             priority
             sizes="52vw"
             className="object-contain object-bottom"
-          />
+          /> */}
         </div>
       </section>
     </main>
